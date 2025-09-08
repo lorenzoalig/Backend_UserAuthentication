183512d2a4aaccbfe03d884cdaf70fba0d7f9a80
lorenzoalig <lorenzoalig@gmail.com>
2025-09-08 18:10:19 -0300

    First commit

---
+++ src/modules/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user.module';
+import { AuthModule } from './auth.module';
+
+@Module({
+  imports: [UserModule, AuthModule]
+})
+export class AppModule {}
